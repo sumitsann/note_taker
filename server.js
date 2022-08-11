4046a67c94ec07d742412cb754e153220cdcce6b
Sumit Ojha <sumit.ojha.dev@gmail.com>
2022-08-11 09:16:00 -0500

    Fixes in post and delete functions in server.js file

--- server.js
+++ server.js
@@ -1,123 +1,71 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
 const fs = require("fs");
 const path = require("path");
 
-const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
+const notesList = require("./db/db.json");
 
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
+app.use(express.static("public"));
 
-const PORT = process.env.PORT || 3001;
-
-const { tasks } = require("./Develop/db/db.json");
-
-function filterByQuery(query, tasksArray) {
-  let filteredResults = tasksArray;
-  if (query.title) {
-    filteredResults = filteredResults.filter(
-      (task) => task.title === query.title
-    );
-  }
-  if (query.text) {
-    filteredResults = filteredResults.filter(
-      (task) => task.text === query.text
-    );
-  }
-
-  return filteredResults;
-}
-
-function findById(id, tasksArray) {
-  const result = tasksArray.filter((task) => task.id === id)[0];
-  return result;
-}
-
-function createNewTask(body, tasksArray) {
-  console.log(body);
-  // our function's main code will go here!
-
-  const task = body;
-  tasksArray.push(task);
-
-  fs.writeFileSync(
-    path.join(__dirname, "./Develop/db/db.json"),
-    JSON.stringify({ tasks: tasksArray }, null, 2)
-  );
-
-  // return finished code to post route for response
-  return task;
-}
-function validateTask(task) {
-  if (!task.title || typeof task.title !== "string") {
-    return false;
-  }
-  if (!task.text || typeof task.text !== "string") {
-    return false;
-  }
-
-  return true;
-}
+app.get("/api/notes", (req, res) => {
+  res.json(notesList.slice(1));
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/notes", function (req, res) {
+app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("/api/tasks", (req, res) => {
-  let results = tasks;
-  console.log(req.query);
-
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/api/tasks/:id", (req, res) => {
-  const result = findById(req.params.id, tasks);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
+function createNewNote(body, notesArrayElements) {
+  const newNote = body;
+  notesArrayElements.push(newNote);
+
+  notesArrayElements.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(notesArrayElements, null, 2)
+  );
+  return newNote;
+}
+
+app.post("/api/notes", (req, res) => {
+  const newNote = createNewNote(req.body, notesList);
+  res.json(newNote);
 });
 
-app.post("/api/tasks", (req, res) => {
-  req.body.id = tasks.length.toString();
-  console.log(req.body);
-  if (!validateTask(req.body)) {
-    res.status(400).send("The task is not properly formatted.");
-  } else {
-    const task = createNewTask(req.body, tasks);
-    res.json(task);
+function deleteNoteByID(id, notesArrayElements) {
+  for (let i = 0; i < notesArrayElements.length; i++) {
+    let note = notesArrayElements[i];
+
+    if (note.id == id) {
+      notesArrayElements.splice(i, 1);
+      fs.writeFileSync(
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify(notesArrayElements, null, 2)
+      );
+
+      break;
+    }
   }
-});
+}
 
-app.delete("/api/tasks/:id", (req, res) => {
-  let savedTasks = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf-8"));
-  taskId = req.params.id;
-  let newID = 0;
-
-  console.log("Deleting task with the ID: " + taskId);
-
-  savedTasks = savedTasks.filter((currentTask) => {
-    return currentTask.id != taskId;
-  });
-  for (currentTask of savedTasks) {
-    currentTask.id = newID.toString();
-    newID++;
-  }
-
-  fs.writeFileSync("./Develop/db/db.json", JSON.stringify(savedTasks));
-  res.json(savedTasks);
+app.delete("/api/notes/:id", (req, res) => {
+  deleteNoteByID(req.params.id, notesList);
+  res.json(true);
 });
 
 app.listen(PORT, () => {
-  console.log("App listening on PORT: " + PORT);
+  console.log("API server now on port: " + PORT);
 });
